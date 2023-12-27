0798a378060d2bfc8e89ce987e583c6e229e04df
Sadullah GOVENC <48141225+SadullahGovenc@users.noreply.github.com>
2023-12-28 00:48:26 +0300

    .

--- Stack Game/Assets/Scripts/StackGame.cs
+++ Stack Game/Assets/Scripts/StackGame.cs
@@ -54,14 +54,30 @@ public class StackGame : MonoBehaviour
 
     void ColorMesh(Mesh mesh)
     {
-        Vector3[] vertices =mesh.vertices;
+        Vector3[] vertices = mesh.vertices;
         Color32[] colors = new Color32[vertices.Length];
 
         float f = Mathf.Sin(scoreCount * 0.25f);
 
-        for(int i = 0;i < vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
-            colors[i] = Lerp4(gameColors[0], gameColors[1], gameColors[2], gameColors[3]);
+            colors[i] = Lerp4(gameColors[0], gameColors[1], gameColors[2], gameColors[3], f);
+        }
+
+        Color32 Lerp4(Color32 a, Color32 b, Color32 c, Color32 d , float t)
+        {
+            if (t < 0.33f)
+            {
+                return Color.Lerp(a, b, t / 0.33f);
+            }
+            else if (t < 0.66f)
+            {
+                return Color.Lerp(b, c, (t - 0.33f) / 0.33f);
+            }
+            else
+            {
+                return Color.Lerp(c, d, (t - 0.66f) / 0.66f);
+            }
         }
     }
 }
