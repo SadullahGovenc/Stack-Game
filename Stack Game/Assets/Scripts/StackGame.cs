02929d5f6f95bc128c9f9d492f02b56c075fc0a9
Sadullah GOVENC <48141225+SadullahGovenc@users.noreply.github.com>
2024-01-03 04:00:34 +0300

    Game completed

--- Stack Game/Assets/Scripts/StackGame.cs
+++ Stack Game/Assets/Scripts/StackGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StackGame : MonoBehaviour
@@ -31,11 +32,25 @@ public class StackGame : MonoBehaviour
     private bool gameOver = false;
 
     private Vector3 desiredPosition;
-    private Vector3 lastTilePosition2;
+    private Vector3 lastTilePosition;
 
 
     void Start()
     {
+        
+            theStack = new GameObject[transform.childCount];
+            stackMat = new Material(Shader.Find("Standard")); // Malzeme atamasý yapýlýyor.
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                theStack[i] = transform.GetChild(i).gameObject;
+                ColorMesh(theStack[i].GetComponent<MeshFilter>().mesh);
+            }
+            stackIndex = transform.childCount - 1;
+        
+
+
+        ///sorun için çözüm deniyorum
         theStack = new GameObject[transform.childCount];
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -44,6 +59,10 @@ public class StackGame : MonoBehaviour
         }
         stackIndex = transform.childCount - 1;
 
+        if (stackMat == null)
+        {
+            Debug.Log("bossss");
+        }
     }
 
     // Update is called once per frame
@@ -53,7 +72,17 @@ public class StackGame : MonoBehaviour
             return;
         if (Input.GetMouseButtonDown(0))
         {
+            if (PlaceTile())
+            {
 
+                SpawnTile();
+                scoreCount++;
+            }
+            else
+            {
+                // end of game
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
         }
         MoveTile();
 
@@ -104,6 +133,156 @@ public class StackGame : MonoBehaviour
         {
             theStack[stackIndex].transform.localPosition = new Vector3(secondaryPosition, scoreCount, Mathf.Sin(tileTransition) * BOUND_Size);
         }
+    } // MOVE TÝLE
+
+    bool PlaceTile()
+    {
+        Transform t = theStack[stackIndex].transform;
+
+        if (isMovingX)
+        {
+            float deltaX = lastTilePosition.x - t.position.x;
+            if (Mathf.Abs(deltaX) > ERROR_Margin)
+            {
+                combo = 0;
+
+                stackBounds.x -= Mathf.Abs(deltaX);
+                if (stackBounds.x <= 0)
+                {
+                    return false;
+                }
+
+                float middle = lastTilePosition.x + t.localPosition.x / 2f;
+                t.localScale = new Vector3(stackBounds.x, 1f, stackBounds.y);
+
+                // part fall down
+                CreateRubble(
+                    new Vector3((t.position.x > 0) ? t.position.x + (t.localScale.x / 2f) : t.position.x - (t.localScale.x / 2f), t.position.y, t.position.z),
+                                    new Vector3(Mathf.Abs(deltaX), 1f, t.localScale.z));
+
+                t.localPosition = new Vector3(middle - (lastTilePosition.x / 2f), scoreCount, lastTilePosition.z);
+
+            }
+            else
+            {
+                if (combo > COMBO_Start_Gain)
+                {
+                    stackBounds.x += STACK_Bounds_Gain;
+                    if (stackBounds.x > BOUND_Size)
+                        stackBounds.x = BOUND_Size;
+                    float middle = lastTilePosition.x + t.localPosition.x / 2f;
+
+                    t.localScale = new Vector3(stackBounds.x, 1f, stackBounds.y);
+                    t.localPosition = new Vector3(middle - (lastTilePosition.x / 2f), scoreCount, lastTilePosition.z);
+
+                }
+
+                combo++;
+                t.localPosition = new Vector3(lastTilePosition.x, scoreCount, lastTilePosition.z);
+
+            }
+
+            
+        }
+        else
+        {
+            float deltaZ = lastTilePosition.z - t.position.x;
+            if (Mathf.Abs(deltaZ) > ERROR_Margin)
+            {
+                combo = 0;
+                stackBounds.y -= Mathf.Abs(deltaZ);
+                if (stackBounds.y <= 0)
+                    return false;
+
+                float middle = lastTilePosition.z + t.localPosition.z / 2f;
+                t.localScale = new Vector3(stackBounds.x, 1f, stackBounds.y);
+
+                CreateRubble(
+                new Vector3(t.position.x, t.position.y, (t.position.z > 0) ? t.position.z + (t.localScale.z / 2f) : t.position.z - (t.localScale.z / 2f)),
+                new Vector3(t.localScale.x, 1f, Mathf.Abs(deltaZ)));
+
+                t.localPosition = new Vector3(lastTilePosition.x / 2f, scoreCount, middle - (lastTilePosition.z / 2f));
+
+
+
+            }
+            else
+            {
+                if (combo > COMBO_Start_Gain)
+                {
+                    stackBounds.y += COMBO_Start_Gain;
+                    if (stackBounds.y > BOUND_Size)
+                    {
+                        stackBounds.y = BOUND_Size;
+                    }
+                    float middle = lastTilePosition.z + t.localPosition.z / 2f;
+                    t.localScale = new Vector3(stackBounds.x, 1f, stackBounds.y);
+                    t.localPosition = new Vector3(lastTilePosition.x / 2f, scoreCount, middle - (lastTilePosition.z / 2f));
+                }
+
+                combo++;
+                t.localPosition = new Vector3(lastTilePosition.x, scoreCount, lastTilePosition.z);
+
+            }
+
+
+
+        } //  ELSE if moving on Axis
+
+
+        secondaryPosition = (isMovingX ? lastTilePosition.x : lastTilePosition.z);
+        isMovingX = !isMovingX;
+
+
+        return true;
+    } // place tile
+
+    void CreateRubble(Vector3 pos, Vector3 scale)
+    { /*
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = pos;
+        go.transform.localScale = scale;
+        go.AddComponent<Rigidbody>();
+        go.AddComponent<MeshRenderer>().material = stackMat;
+        ColorMesh(go.GetComponent<MeshFilter>().mesh);
+        */
+
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = pos;
+        go.transform.localScale = scale;
+        go.AddComponent<Rigidbody>();
+        // MeshRenderer'ý kontrol et ve eðer yoksa ekle
+        MeshRenderer meshRenderer = go.GetComponent<MeshRenderer>();
+        if (meshRenderer == null)
+        {
+            meshRenderer = go.AddComponent<MeshRenderer>();
+        }
+
+        meshRenderer.material = stackMat; // stackMat malzemesini atadýk
+
+        ColorMesh(go.GetComponent<MeshFilter>().mesh);
+
+    } // Create Rubble
+
+    void SpawnTile()
+    {
+        lastTilePosition = theStack[stackIndex].transform.localPosition;
+        stackIndex--;
+
+        if (stackIndex < 0)
+        {
+            stackIndex = transform.childCount - 1;
+        }
+
+        desiredPosition = Vector3.down * scoreCount;
+
+        theStack[stackIndex].transform.localPosition = new Vector3(0f, scoreCount, 0f);
+        theStack[stackIndex].transform.localScale = new Vector3(stackBounds.x ,1f, stackBounds.y);
+
+        ColorMesh(theStack[stackIndex].GetComponent<MeshFilter>().mesh);
+
     }
+
+
 
 } // CLASS
